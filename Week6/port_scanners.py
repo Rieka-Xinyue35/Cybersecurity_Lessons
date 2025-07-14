74b195c145dfd7c21b93883d993e2ba9136b90a7
Rieka-Xinyue35 <luuluansahabiesi@gmail.com>
2025-07-14 12:42:16 +0000

    Port_scanner

---
+++ Week6/port_scanners.py
@@ -0,0 +1,16 @@
+import socket
+
+target_ip_address = '192.168.1.61'
+list_of_ports =  [123,445,80,8080,25]
+print("Scanning open ports...")
+
+for port in list_of_ports:
+    scanner_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    scanner_socket.settimeout(1)
+    scanned_port = scanner_socket.connect_ex((target_ip_address,port))
+    if scanned_port == 0:
+        print(f"Port {port} is Opened")
+    else:
+        print(f"Port {port} is Closed")
+
+scanner_socket.close()
